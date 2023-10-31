c0130d4dce4f7ff1be82253583b59e4e927a2fc6
Jean Claude Nimi <nimimingi2000@yahoo.fr>
2023-10-31 14:47:35 +0100

    ok

--- js/index.js
+++ js/index.js
@@ -1,5 +1,6 @@
+import { fetchData } from "./comonlib.js";
 var btn_submit = document.getElementById("btnsubmit");
-btn_submit.addEventListener("click", function(e) {
+btn_submit.addEventListener("click", async function(e) {
   e.preventDefault();
   var v_password = document.getElementById("password").value;
   var v_userName = document.getElementById("username").value;
@@ -9,36 +10,25 @@ btn_submit.addEventListener("click", function(e) {
   };
 
   console.log("Body: ", JSON.stringify(obj))
-  
-  fetch("https://cdwebapsvr.fincadrc.com:8002/users/login2", {
-    method: "POST",
-    body: JSON.stringify(obj),
-    mode: 'no-cors', // no-cors, *cors, same-origin
-    cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
-    credentials: 'same-origin',
-    headers: {
-        'Content-Type': 'application/json',
-        'Access-Control-Allow-Origin': '*'
-    },
-  })
-    .then(response => response.json())
-    .then(result => {
-        console.log( JSON.stringify(result))
-     
-        if(result !== null && result.status === 'success'){
-            let userId = result.data.user_id;
-            let fullname = result.data.fullname;
-            let branch = result.data.branch_name;
-            let group = result.data.user_group;
-            //set session info
-            sessionStorage.setItem("sessionid", "xxx"); // will change it to jwt token 
-            //sessionStorage.setItem("group", JSON.stringify(result.data.user_group));
-            window.location.replace(`loan-request.htm?userid=${userId}&name=${fullname}&branch=${branch}&group=${group}`);
-        }
-    }).catch(error => {
-        // handle the error
-        console.log(error)
-    })
-    ;
 
+
+  try {
+    url = `/users/login2`;
+    result = await fetchData(url, 'POST');
+    if(result !== null && result.status === 'success'){
+      if(result !== null && result.status === 'success'){
+        let userId = result.data.user_id;
+        let fullname = result.data.fullname;
+        let branch = result.data.branch_name;
+        let group = result.data.user_group;
+        //set session info
+        sessionStorage.setItem("sessionid", "xxx"); // will change it to jwt token 
+        //sessionStorage.setItem("group", JSON.stringify(result.data.user_group));
+        window.location.replace(`loan-request.htm?userid=${userId}&name=${fullname}&branch=${branch}&group=${group}`);
+      }
+    }
+  } catch(error) {
+    // handle the error
+    console.log(error)
+  }
 });
